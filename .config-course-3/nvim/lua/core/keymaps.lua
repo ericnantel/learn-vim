b3b53a6ebc38c89f8945b5dec6b8cb67a8ae95e3
eric <eric@voxelfarm.com>
2025-05-09 14:19:30 -0400

    Updated Neovim Config (.config-course-3)

--- .config-course-3/nvim/lua/core/keymaps.lua
+++ .config-course-3/nvim/lua/core/keymaps.lua
@@ -4,8 +4,8 @@ vim.g.maplocalleader = " "
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit Insert Mode" })
 vim.keymap.set("v", "jk", "<ESC>", { desc = "Exit Visual Mode" })
 
-vim.keymap.set("n", "+", "<C-a>", { desc = "Increment Number" })
-vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement Number" })
+vim.keymap.set("n", "+", "<C-a>", { desc = "Increment Number", remap = false })
+vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement Number", remap = false })
 
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split Window Vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split Window Horizontally" })

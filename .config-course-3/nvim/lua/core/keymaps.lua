8b5303f14dc8621ce9c39a6c4f89bd50ff3d79a2
eric <eric@voxelfarm.com>
2025-05-09 14:23:25 -0400

    Updated Neovim Config (.config-course-3)

--- .config-course-3/nvim/lua/core/keymaps.lua
+++ .config-course-3/nvim/lua/core/keymaps.lua
@@ -11,3 +11,9 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split Window Vertically" }
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split Window Horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make Splits Equal Size" })
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close Current Split" })
+vim.keymap.set("n", "<leader>s<Left>", "<C-w>5<", { desc = "Decrement Current Split Width" })
+vim.keymap.set("n", "<leader>s<Right>", "<C-w>5>", { desc = "Increment Current Split Width" })
+vim.keymap.set("n", "<leader>s<Up>", "<C-w>-", { desc = "Decrement Current Split Height" })
+vim.keymap.set("n", "<leader>s<Down>", "<C-w>+", { desc = "Increment Current Split Height" })
+
+vim.keymap.set("n", "<leader>nh", "<cmd>nohlsearch<CR>", { desc = "Clear Search Highlight" })
